484449c4e2983b8e1ac0a086f980214804286d65
Yukang Wu <108606229+1315701963@users.noreply.github.com>
2025-10-30 11:10:49 +0800

    Update sft_stage4_train.sh

--- finetune/sft_stage4_train.sh
+++ finetune/sft_stage4_train.sh
@@ -6,7 +6,7 @@ llamafactory-cli train \
     --lora_target "q_proj,k_proj,v_proj,o_proj,gate_proj,up_proj,down_proj" \
     --lora_dropout 0.05 \
     \
-    --model_name_or_path /home/zf1/WuYukang/AIstorian/model/lora_export_stage3 \
+    --model_name_or_path ./model/lora_export_stage3 \
     --trust_remote_code True \
     --image_max_pixels 262144 \
     --video_max_pixels 16384 \
@@ -20,7 +20,7 @@ llamafactory-cli train \
     --dataloader_num_workers 2 \
     --val_size 0.08 \
     \
-    --output_dir /home/zf1/WuYukang/AIstorian/model/lora/sft_stage4 \
+    --output_dir ./model/lora/sft_stage4 \
     --logging_steps 5 \
     --save_steps 1000 \
     --plot_loss True \
