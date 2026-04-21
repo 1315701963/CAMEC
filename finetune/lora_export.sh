35e6767f4a8ea9664e545efbeec74d9f63be4c6d
Yukang Wu <108606229+1315701963@users.noreply.github.com>
2026-04-21 22:03:14 +0800

    Update lora_export.sh

--- finetune/lora_export.sh
+++ finetune/lora_export.sh
@@ -1,6 +1,6 @@
 llamafactory-cli export \
-    --model_name_or_path ./model/lora_export_stage3 \
-    --adapter_name_or_path ./model/lora/sft_stage4 \
+    --model_name_or_path ./model/qwen3-8b \
+    --adapter_name_or_path ./model/lora/sft_output \
     --template qwen3 \
     --finetuning_type lora \
     --trust_remote_code True \
